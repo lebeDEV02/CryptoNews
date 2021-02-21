5fe7fbc920ded091a6b85e1c750b00d042c67f17
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-02-21 21:00:08 +0300

    added pseudoslider + 1/2 articles[static yet]

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,40 @@
 $(function () {
-
+	const slidesCounter = document.querySelectorAll('.articles__item'),
+	activeBlock = document.querySelector('.articles__items--active'),
+	numbersOfBlock = document.querySelectorAll('.articles__items')
+	leftAngle = document.querySelector('.articles__angle-left'),
+	rightAngle = document.querySelector('.articles__angle-right');
+	rightAngle.style.transition = 'all .4s';
+	leftAngle.style.transition = 'all .4s';
+		syncSlider = function(){
+		if (counter === 0){
+			leftAngle.style.opacity = "0.4";
+		} else{
+			leftAngle.style.opacity = "1";
+		}
+		if (counter === numbersOfBlock.length - 1){
+			rightAngle.style.opacity = "0.4";
+		} else{
+			rightAngle.style.opacity = "1";
+		}
+	}
+	let counter = 0;
+	syncSlider();
+	leftAngle.addEventListener('click', function () {
+		if(counter !=0){
+		numbersOfBlock[counter].classList.remove('articles__items--active');
+		counter -= 1;
+		numbersOfBlock[counter].classList.add('articles__items--active');	
+		syncSlider();
+		}
+		});
+	rightAngle.addEventListener('click', function () {
+		if(counter !=numbersOfBlock.length - 1){
+		numbersOfBlock[counter].classList.remove('articles__items--active');
+		counter += 1;
+		numbersOfBlock[counter].classList.add('articles__items--active');
+		syncSlider();              
+		}
+	});
+	console.log(numbersOfBlock.length);
 })
