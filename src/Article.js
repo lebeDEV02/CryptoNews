185d1c5d83e7bd410fc14fc6ae5aeb77d8fb7050
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-03-01 22:00:59 +0300

    added article page

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,32 @@
 import { useParams } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+import useFetch from './useFetch';
 
 const Article = () => {
 	const { id } = useParams();
+	const { data: article, isPending, error } = useFetch('http://localhost:8000/articles/' + id);
 	return (
-		<h1>This article with id: {id}</h1>
+		<div className="App">
+			<Header></Header>
+			<main className="main">
+				<div className="container">
+					<div className="article-details">
+						{isPending && <div>Loading...</div>}
+						{error && <div>{error}</div>}
+						{article && (
+							<article>
+								<h2 className="article-details__title">{article.title}</h2>
+								<img className="article-details__img" src={article.coverImage} />
+								<p className="article-details__content" >{article.content}</p>
+							</article>
+						)}
+					</div>
+				</div>
+			</main>
+			<Footer></Footer>
+		</div>
+
 	);
 }
 
